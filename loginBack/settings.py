77217d6f1b3c7ac29b944f58edd138039c285f02
BlisS <contacto@fixter.org>
2017-08-21 16:21:28 -0500

    face backend

--- loginBack/settings.py
+++ loginBack/settings.py
@@ -153,7 +153,9 @@ REST_FRAMEWORK = {
 
 AUTHENTICATION_BACKENDS = (
     # Twitter
-    'social_core.backends.twitter.TwitterOAuth',
+    # 'social_core.backends.twitter.TwitterOAuth',
+    'social_core.backends.facebook.FacebookAppOAuth2',
+    'social_core.backends.facebook.FacebookOAuth2',
    'rest_framework_social_oauth2.backends.DjangoOAuth2',
    'django.contrib.auth.backends.ModelBackend',
 )
